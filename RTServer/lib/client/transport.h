0bf1e07332905fc199ae98c54a1b2a86e75afcb5
bishenghua <net.bsh@gmail.com>
2014-04-07 22:24:17 +0800

    增加数据解析类

---
+++ RTServer/lib/client/transport.h
@@ -0,0 +1,15 @@
+#include "const.h"
+
+//定义传输数据结构
+typedef struct _RTS_TRANSPORT_DATA {
+	int id;
+	int toid;
+	char action[MAX_ACTION_LENGTH + 1];
+	char name[MAX_NAME_LENGTH + 1];
+	char password[MAX_PASSWORD_LENGTH + 1];
+	char token[MAX_TOKEN_LENGTH + 1];
+	wchar_t content[MAX_CONTENT_LENGTH + 1];
+}_RTS_TRANSPORT_DATA;
+
+extern int RTS_transport_data_parse(const char *buf, _RTS_TRANSPORT_DATA *_rts_transport_data);
+extern _RTS_TRANSPORT_DATA RTS_transport_data_init();
